9e77954499e8aa3d53226787519cacc93b9b499d
chainsx <chainsx@outlook.com>
2024-09-06 20:08:25 +0800

    fix syterkit path

--- scripts/lib/bootloader/bootloader-sunxi-syterkit.sh
+++ scripts/lib/bootloader/bootloader-sunxi-syterkit.sh
@@ -11,10 +11,12 @@ build_bootloader(){
   cd ${BL_CONFIG} && mkdir build-${BOARD} && cd build-${BOARD}
   cmake -DCMAKE_BOARD_FILE=${BOARD}.cmake -DCMAKE_BUILD_TYPE=Release ..
   make -j$(nproc)
+  cd ${workspace}
 }
 
 apply_bootloader(){
   BOARD=$1
+  cd ${workspace}
   source ../boards/${BOARD}.conf
   if [ -d ${workspace}/bootloader-${BOARD} ];then rm -rf ${workspace}/bootloader-${BOARD}; fi
 
@@ -23,10 +25,15 @@ apply_bootloader(){
   cp ${workspace}/../target/boot/extlinux.conf ${workspace}/bootloader-${BOARD}/extlinux
   sed -i "s|BOARD_NAME|${DEVICE_DTS}|g" ${workspace}/bootloader-${BOARD}/extlinux/extlinux.conf
   sed -i "s|BOOTARGS|${BOOTARGS}|g" ${workspace}/bootloader-${BOARD}/extlinux/extlinux.conf
-
-  cp board/${BOARD}/${SYTERKIT_TYPE}/${SYTERKIT_TYPE}_bin_card.bin ${workspace}/bootloader.bin
-  cp ../board/${BOARD}/${SYTERKIT_TYPE}/bl31/bl31.bin ${workspace}/bootloader-${BOARD}/bl31.bin
-  cp ../board/${BOARD}/${SYTERKIT_TYPE}/scp/scp.bin ${workspace}/bootloader-${BOARD}/scp.bin
-  cp ../board/${BOARD}/${SYTERKIT_TYPE}/splash/splash.bin ${workspace}/bootloader-${BOARD}/splash.bin
+  
+  cp ${workspace}/${BL_CONFIG}/build-${BOARD}/board/${BOARD}/${SYTERKIT_TYPE}/${SYTERKIT_TYPE}_bin_card.bin \
+  	${workspace}/bootloader.bin
+  cp ${workspace}/${BL_CONFIG}/board/${BOARD}/${SYTERKIT_TYPE}/bl31/bl31.bin \
+  	${workspace}/bootloader-${BOARD}/bl31.bin
+  cp ${workspace}/${BL_CONFIG}/board/${BOARD}/${SYTERKIT_TYPE}/scp/scp.bin \
+  	${workspace}/bootloader-${BOARD}/scp.bin
+  cp ${workspace}/${BL_CONFIG}/board/${BOARD}/${SYTERKIT_TYPE}/splash/splash.bin \
+  	${workspace}/bootloader-${BOARD}/splash.bin
+  	
   echo "${BOARD}" > ${workspace}/bootloader-${BOARD}/.done
 }
